d47b4e035fa726f9dc6fea6578baf0377d3bedde
Alan <alanas.vaiciulis@gmail.com>
2020-04-09 23:15:18 +0300

    add todo toggling and refactor fetching to helper functions

--- client/src/TodoList.js
+++ client/src/TodoList.js
@@ -1,8 +1,7 @@
 import React, {Component} from 'react';
 import TodoItem from './TodoItem';
 import TodoForm from './TodoForm';
-
-const APIURL = '/api/todos/';
+import * as apiCalls from './api';
 
 class TodoList extends Component {
     constructor(props){
@@ -17,78 +16,29 @@ class TodoList extends Component {
         this.loadTodos();
     }
 
-    loadTodos() {
-        fetch(APIURL)
-        .then(res => {
-            if(!res.ok) {
-                if(res.status >= 400 & res.status < 500) {
-                    return res.json().then(data => {
-                        let err = {errorMessage: data.message};
-                        throw err;
-                    }) 
-                } else {
-                    let err = {errorMessage: "Error. API not responding."};
-                    throw err;
-                }
-            }
-            return res.json();
-        })
-        .then(todos => this.setState({todos}))
-        .catch(function(err){
-            console.log(err);
-        })
+    async loadTodos() {
+        let todos = await apiCalls.getTodos();
+        this.setState({todos});
     }
 
-    addTodo(val){
-        fetch(APIURL, {
-            method: 'post',
-            headers: new Headers({
-                'Content-Type': 'application/json'
-            }),
-            body: JSON.stringify({name: val})
-        })
-        .then(res => {
-            if(!res.ok) {
-                if(res.status >= 400 & res.status < 500) {
-                    return res.json().then(data => {
-                        let err = {errorMessage: data.message};
-                        throw err;
-                    }) 
-                } else {
-                    let err = {errorMessage: "Error. API not responding."};
-                    throw err;
-                }
-            }
-            return res.json();
-        })
-        .then(newTodo => {
-            this.setState({todos: [...this.state.todos, newTodo]}) 
-        })
+    async addTodo(val){
+        let newTodo = await apiCalls.addTodo(val);
+        this.setState({todos: [...this.state.todos, newTodo]}) 
+        
     }
 
-    deleteTodo(id) {
-        const deleteURL = APIURL + id;
-        fetch(deleteURL, {
-            method: 'delete'
-        })
-        .then(res => {
-            if(!res.ok) {
-                if(res.status >= 400 & res.status < 500) {
-                    return res.json().then(data => {
-                        let err = {errorMessage: data.message};
-                        throw err;
-                    }) 
-                } else {
-                    let err = {errorMessage: "Error. API not responding."};
-                    throw err;
-                }
-            }
-            return res.json();
-        })
-        .then(() => {
-            const todos = this.state.todos.filter(todo => todo._id !== id)
-            this.setState({todos: todos}) 
-        })
+    async deleteTodo(id) {
+        await apiCalls.removeTodo(id);
+        const todos = this.state.todos.filter(todo => todo._id !== id)
+        this.setState({todos: todos}) 
+    }
+
+    async toggleTodo(todo) {
+        let updatedTodo = await apiCalls.toggleTodo(todo);
+        const todos = this.state.todos.map(task => 
+            (task._id === updatedTodo._id) ?
+                {...task, completed: !task.completed} : task)
+        this.setState({todos: todos}) 
     }
 
     render() {
@@ -97,6 +47,7 @@ class TodoList extends Component {
                 key={task._id}
                 {...task}
                 onDelete={this.deleteTodo.bind(this, task._id)}
+                onToggle={this.toggleTodo.bind(this, task)}
             />
         ));
         return (
